a72b6412cfead1616f20f84a26b3d56ef351ef3b
Ammar Qammaz <ammarkov@gmail.com>
2025-11-20 13:59:59 +0200

    ...

--- build.sh
+++ build.sh
@@ -3,14 +3,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-sudo apt-get install dkms
+sudo apt-get install git dkms
+
+#Assuming we just got this script without the aic8800 repo..
+#git clone https://github.com/geniuskidkanyi/aic8800
+#cd aic8800
 
 #Also include aic8800D80
 git clone https://github.com/jeremyb31/aic8800D80.git
 mv aic8800D80/aic8800D80 blobs/
-
-sudo mkdir /lib/firmware/aic8800D80
-sudo cp aic8800D80/* /lib/firmware/aic8800D80/
 
 sudo cp -r src /usr/src/aic8800-1.0.5
 sudo cp -r blobs/* /usr/lib/firmware/
